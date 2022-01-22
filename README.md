46714979f73f818957755263606711ba45eabe12
Callum Mackenzie <alexxander1611@gmail.com>
2022-01-22 16:33:18 -0700

    Initial commit.

---
+++ README.md
@@ -0,0 +1,52 @@
+# Delete Lambda Layers Action
+
+This action takes given aws credentials and deletes lambda layers from oldest to newest until the provided amount remains
+
+## Usage
+This example would remove dependencies from oldest to newest until there were 6 versions remaining.
+```yaml
+name: Trim AWS Lambda Layers
+
+on:
+  release:
+    types: [created]
+  push:
+    branches:
+      - master
+
+jobs:
+  update_lambda_layers:
+  	name: Trim layer versions
+    runs-on: ubuntu-latest
+    steps:
+      - name: Trim versions
+        uses: callummackenzie/latest-lambda-layer-action@LATEST_VERSION
+        with:
+          aws_access_key_id: ${{ secrets.AWS_ACCESS_KEY_ID }}
+          aws_secret_access_key: ${{ secrets.AWS_SECRET_ACCESS_KEY }}
+          aws_region: ${{ secrets.AWS_REGION }
+          layer_name: SomeAWSLambdaLayer
+		  version_keep_count: 6
+```
+## Parameters
+- aws_access_key_id
+  - Required
+  - The aws access key id for an IAM user
+- aws_secret_access_key
+  - Required
+  - The secret access key for the same IAM user used in aws_access_key_id
+- aws_region
+  - Required
+  - The aws region in which the desired lambda function is located such as us-east-1
+- layer_name
+  - Required
+  - The layer function name to be updated (not the ARN)
+- version_keep_count:
+  - Required
+  - The number of latest versions to keep
+  - Default is 4
+
+## AWS Permissions Policy
+The AWS IAM user this action uses must have permissions to:
+- **lambda:ListLayerVersions** for the desired layer
+- **lambda:DeleteLayerVersion** for the desired layer
---
+++ index.js
@@ -0,0 +1,48 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+import Lambda from 'aws-sdk/clients/lambda';
+
+const run = async () => {
+	try {
+		// Extract name
+		const lambda_layer_name = core.getInput('layer_name', { required: true });
+		const vesions_to_keep = core.getInput('version_keep_count', { required: true });
+		const lambda_config = {
+			accessKeyId: core.getInput('aws_access_key_id', { required: true }),
+			apiVersion: '2015-03-31',
+			maxRetries: 2,
+			region: core.getInput('aws_region', { required: true }),
+			secretAccessKey: core.getInput('aws_secret_access_key', { required: true }),
+			sslEnabled: true
+		};
+
+		// Create lambda API client
+		const lambda = new Lambda(lambda_config);
+
+		core.info("Requesting lambda versions");
+		const lambda_versions_request = await lambda.listLayerVersions({
+			LayerName: lambda_layer_name
+		}).promise();
+
+		const sorted_versions = lambda_versions_request
+			.listLayerVersions.sort((a, b) => a.Version - b.Version);
+		const versions_to_delete = sorted_versions.length - versions_to_keep;
+
+		core.info("Deleting "
+			+ (versions_to_delete < 0 ? 0 : versions_to_delete)
+			+ " old versions");
+		for (let i = 0; i < versions_to_delete; ++i) {
+			await lambda.deleteLayerVersion({
+				LayerName: lambda_layer_name,
+				VersionNumber: sorted_versions[i].Version
+			}).promise();
+			core.info("Deleted version (" + (i + 1) + "/" + versions_to_delete + ")");
+		}
+
+		core.info("Layer version deletions success");
+	} catch (error) {
+		core.setFailed(error.message);
+	}
+};
+
+run();
